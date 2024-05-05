dcb373902713c24d1a8d2763c507a1057931ad9b
MJahidH <m.jahid.h@hotmail.com>
2024-05-05 22:47:25 +0100

    Player class created

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const c = canvas.getContext(`2d`);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
-// creates a black rectangle that fills the whole canvas 
+// creates a black rectangle that fills the whole canvas
 c.fillStyle = "black";
-c.fillRect(0,0,canvas.width,canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
 
+class Player {
+  constructor({ position, velocity, color, radius }) {
+    this.position = position;
+    this.velocity = velocity;
+  }
+}
 
+const plaer = new Player({
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
 
-
+console.log(plaer)
