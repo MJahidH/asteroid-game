f6894ee55f6bad53f398ab04645db1d781ab204d
MJahidH <m.jahid.h@hotmail.com>
2024-05-06 22:58:06 +0100

    spaceship moves forward functionlity added, assigned ot W key

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // creates a black rectangle that fills the whole canvas
-c.fillStyle = "black";
-c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Player {
   constructor({ position, velocity, color, radius }) {
@@ -15,15 +13,22 @@ class Player {
   }
 
   draw() {
+    c.beginPath();
     c.moveTo(this.position.x + 50, this.position.y);
     c.lineTo(this.position.x - 10, this.position.y - 20);
     c.lineTo(this.position.x - 10, this.position.y + 20);
+    c.closePath();
     c.closePath();
 
     c.strokeStyle = `yellow`;
     c.fillStyle = `yellow`;
     c.stroke();
     c.fill();
+  }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
   }
 }
 
@@ -32,19 +37,38 @@ const player = new Player({
   velocity: { x: 0, y: 0 },
 });
 
-player.draw();
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
 
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  if (keys.w.pressed) player.velocity.x = 1;
+}
+
+animate();
 window.addEventListener(`keydown`, (event) => {
-
   switch (event.code) {
     case `KeyW`:
-      console.log(`hello world `)
-    break 
+      keys.w.pressed = true;
+      break;
     case `KeyA`:
-      console.log(`hello world `)
-    break 
+      console.log(`hello world `);
+      break;
     case `KeyD`:
-      console.log(`hello world `)
-    break 
+      console.log(`hello world `);
+      break;
   }
 });
